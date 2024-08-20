03a5fbc40a1ba643b92675206d7dd4c341d24a35
dominik <105545749+dominik-deak@users.noreply.github.com>
2024-08-20 11:56:02 +0100

    added daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 export default function App() {
-	return <h1 className='text-center text-3xl'>Hello world!</h1>;
+	return (
+		<>
+			<h1 className='text-center text-3xl'>Hello world!</h1>
+			<button className='btn btn-error'>Button</button>
+		</>
+	);
 }
